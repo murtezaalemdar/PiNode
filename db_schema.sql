c46326373f9ce457e120381a81f13f078eec42a4
Murteza ALEMDAR <murteza@karakoc.local>
2026-05-25 15:57:26 +0300

    Update db schema to include downtime_minutes

--- db_schema.sql
+++ db_schema.sql
@@ -1,11 +1,3 @@
-CREATE TABLE IF NOT EXISTS "nodes" (
-  "id" TEXT PRIMARY KEY,
-  "name" TEXT,
-  "port_prefix" TEXT,
-  "created_at" TEXT,
-  "auto_restart" INTEGER DEFAULT 0
-);
-CREATE TABLE IF NOT EXISTS "settings" (
-  "key" TEXT PRIMARY KEY,
-  "value" TEXT
-);
+CREATE TABLE nodes 
+                 (id TEXT PRIMARY KEY, name TEXT, port_prefix TEXT, created_at TEXT, auto_restart INTEGER DEFAULT 0, downtime_minutes INTEGER DEFAULT 0);
+CREATE TABLE settings (key TEXT PRIMARY KEY, value TEXT);
